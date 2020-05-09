d858d68e4b37be061ad81346ffbcfebcadcb5023
Arthur Prasniski <prasniski@outlook.com>
2020-05-09 17:59:01 -0300

    commit 2

---
+++ SRC/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '2670',
+  database: 'TaskList',
+  define: {
+    timestamps: true,
+    underscored: true,
+    undersocredAll: true,
+  },
+};
--- SRC/routes.js
+++ SRC/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.get('/teste', async (req, res) =>
+{
+  const user = await User.create({
+    name: 'Usuario',
+    email: 'usuario@mail.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
